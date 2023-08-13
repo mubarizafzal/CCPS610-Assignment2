2afe1b40f121edf352ba2867688c157acd11d351
Mubariz Afzal <mubarizafzal@gmail.com>
2023-08-13 19:30:05 -0400

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,5 +1,11 @@
-<?php     
-  $conn = oci_connect("m8afzal", "11167653", "//oracle12c.scs.ryerson.ca:1521/orcl12c");
+<?php
+  $file = file_get_contents('.passfile'); // there should not be a newline at the end
+  $fileData = explode(':', $file);
+
+  $user = $fileData[0];
+  $pass = $fileData[1];
+
+  $conn = oci_connect($user, $pass, "//oracle12c.scs.ryerson.ca:1521/orcl12c");
 
   if (!$conn) {
       $msg = oci_error();
